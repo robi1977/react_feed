c8dbcf2578d16743a056337ab40c6f9c97c94ef3
Rafal Robi1977 <rafal.robi.nowak@gmail.com>
2020-03-15 13:33:07 +0100

    change App.js to use new components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './styles/foundation.min.css';
+import './styles/custom.css';
+import Routes from './routes';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import MobileHeader from './components/MobileHeader/MobileHeader';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super();
+    this.state={
+      appName: "ReactJS Feed Project",
+      home: false
+    }
+  }
+  render(){
+    return(
+      <div className="off-canvas-wrapper">
+        <div className="off-canvas-wrapper-inner" data-off-canvas-wrapper>
+          <div className="off-canvas-content" dataoff-canvas-content>
+            <MobileHeader name={this.state.appName} />
+            <Header name={this.state.appName} />
+            <Routes name={this.state.appName} />
+            <hr />
+            <Footer />
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
