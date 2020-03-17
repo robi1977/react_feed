b3885a45395edc14c1d989b68e6fa4c773f860a3
Rafal Robi1977 <rafal.robi.nowak@gmail.com>
2020-03-17 23:28:12 +0100

    component HOME, but is somthing what I don't like at it

---
+++ src/components/Home/Home.js
@@ -0,0 +1,153 @@
+import React, {Component} from 'react';
+import {Redirect} from 'react-router-dom';
+import './Home.css';
+import {PostData} from '../../services/PostData';
+//import {UserFeed} from'../UserFeed/UserFeed';
+import {confirmAlert} from 'react-confirm-alert';
+import '../../styles/react-confirm-alerts.css';
+
+class Home extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            data: [],
+            userFeed: '',
+            redirectToReferrer: false,
+            name: ''
+        };
+        this.getUserFeed = this.getUserFeed.bind(this);
+        this.feedUpdate = this.feedUpdate.bind(this);
+        this.onChange = this.onChange.bind(this);
+        this.deleteFeed = this.deleteFeed.bind(this);
+        this.editFeed = this.editFeed.bind(this);
+        this.deleteFeedAction = this.deleteFeedAction.bind(this);
+        this.logout = this.logout.bind(this);
+    }    
+
+    componentDidMount(){
+        if(sessionStorage.getItem('userData')){
+            this.getUserFeed();
+        }else{
+            this.setState({
+                redirectToReferrer: true
+            });
+        }
+    }
+
+    feedUpdate(e){
+        e.preventDefault();
+        let data=JSON.parse(sessionStorage.getItem('userData'));
+        let postData={
+            user_id: data.userData.user_id,
+            feed: this.state.userFeed
+        }
+        if(this.state.userFeed){
+            PostData('feedUpdate', postData).then((result)=>{
+                let responseJson = result;
+                this.setState({
+                    data: responseJson.feedData
+                });
+            });
+        }
+    }
+
+    deleteFeed(e){
+        confirmAlert({
+            title: 'Delete Feed?',
+            message: 'Are you sure to delete this feed?',
+            buttons:[
+                {
+                    label: 'Yes',
+                    onClick: ()=>this.deleteFeedAction(e)
+                },{
+                    label: 'No',
+                    //onClick: ()=>alert('Click No');
+                    //TODO: dlaczego linia powyżej jest zaremowana. sprawdzić dokumentację react-confirm-alert
+                }
+            ]
+        })
+    }
+
+    deleteFeedAction(e){
+        let updateIndex=e.target.getAttribute('value');
+        let feed_id=document.getElementById('del').getAttribute('data');
+        let data=JSON.parse(sessionStorage.getItem('userData'));
+        let postData={
+            user_id: data.userData.user_id,
+            feed_id: feed_id
+        }
+        if(postData){
+            PostData('feedDelete', postData).then((result)=>{
+                this.state.data.splice(updateIndex,1); //TODO: po co skoro nie jest nigdzie przypisywane
+                this.setState({
+                    data: this.state.data //TODO: znów przypisywanie do tego samego tego samego????
+                });
+                if(result.success){
+                    alert(result.success);
+                }else{
+                    alert(result.error);
+                }
+            });
+
+            //TODO: Nie podoba mi się powyższa funkcja. jest trochę namieszana
+        }
+    }
+
+    getUserFeed(){
+        let data=JSON.parse(sessionStorage.getItem('userData'));
+        this.setState({
+            name: data.userData.name
+        });
+        let postData={
+            user_id: data.userData.user_id
+        };
+        if(data){
+            PostData('feed', postData).then((result)=>{
+                let responseJson=result;
+                if(responseJson.feedData){
+                    this.setState({
+                        data: responseJson.feedData
+                    });
+                }else{
+                    alert(result.error);
+                }
+            });
+        }
+    }
+
+    onChange(e){
+        this.setState({
+            userFeed: e.target.value
+        });
+    }
+
+    logout(){
+        sessionStorage.setItem('userData','');
+        sessionStorage.clear();
+        this.setState({
+            redirectToReferrer: true
+        });
+    }
+    render(){
+        if(this.state.redirectToReferrer){
+            return(<Redirect to={'/login'} />);
+        }
+        return (
+            <div className="row" id="Body">
+                <div className="medium-12 columns">
+                    <a href="/#" onClick={this.logout} className="logout">Logout</a>
+                    <form onSubmit={this.feedUpdate} metod="post">
+                        <input name="userFeed" onChange={this.onChange} value={this.state.userFeed} type="text" placeholder="Write your feed here..." />
+                        <input type="submit" value="Post" className="button" onClick={this.feedUpdate} /> 
+                        {
+                            //TODO: sprawdzić procedurę submit w formularzu, czy potrzebny jest podwójny submit i w przycisku i w formularzu
+                        }
+                    </form>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Home;
+
