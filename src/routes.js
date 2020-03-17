baa9424db232a9c807a5b200ab29a72808e1fe66
Rafal Robi1977 <rafal.robi.nowak@gmail.com>
2020-03-17 23:37:45 +0100

    update routes with new components

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 import Welcome from '././components/Welcome/Welcome';
-//import Home from '././components/Home/Home';
-//import Login from '././components/Login/Login';
-//import Singup from '././components/Singup/Singup';
+import Home from '././components/Home/Home';
+import Login from '././components/Login/Login';
+import Singup from '././components/Singup/Singup';
 import NotFound from '././components/NotFound/NotFound';
 
 const Routes=()=>(
     <BrowserRouter>
         <Switch>
             <Route exact path='/' component={Welcome} />
+            <Route path='/home' component={Home} />
+            <Route path='/login' component={Login} />
+            <Route path='/singup' component={Singup} />
             <Route path="*" component={NotFound} />
         </Switch>
     </BrowserRouter>
